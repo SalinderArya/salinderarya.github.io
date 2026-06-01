132d4ce39d5987f0fc2a6ab00ac7cca1b30c52b4
SalinderArya <samajsevak2013@gmail.com>
2026-06-01 21:31:41 +0530

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,1 +1,26 @@
-console.log("Login Module Loaded");
+document
+.getElementById("loginBtn")
+.addEventListener("click", function(){
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+/* SUPER ADMIN */
+
+if(username === "admin" &&
+   password === "admin")
+{
+
+window.location.href =
+"../superadmin/admin.html";
+
+return;
+
+}
+
+alert("Invalid Username Or Password");
+
+});
