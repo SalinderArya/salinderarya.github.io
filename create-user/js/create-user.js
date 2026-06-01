7b835e612f3f8a054038be4b5a75365fc348837d
SalinderArya <samajsevak2013@gmail.com>
2026-06-01 22:39:14 +0530

    Update create-user.js

--- create-user/js/create-user.js
+++ create-user/js/create-user.js
@@ -1,1 +1,97 @@
-console.log("Create User Page Loaded");
+const SUPABASE_URL =
+"https://hjxqtfykngkwmiejeyei.supabase.co";
+
+const SUPABASE_KEY =
+"YOUR_PUBLISHABLE_KEY_HERE";
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+document
+.getElementById("createUserBtn")
+.addEventListener("click", async function () {
+
+    const name =
+    document.getElementById("name").value;
+
+    const mobile =
+    document.getElementById("mobile").value;
+
+    const ko_code =
+    document.getElementById("ko_code").value;
+
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    const confirm_password =
+    document.getElementById("confirm_password").value;
+
+    const user_type =
+    document.getElementById("user_type").value;
+
+    if (
+        !name ||
+        !mobile ||
+        !ko_code ||
+        !username ||
+        !password
+    ) {
+
+        Swal.fire(
+            "Required",
+            "Please fill all fields",
+            "warning"
+        );
+
+        return;
+    }
+
+    if(password !== confirm_password){
+
+        Swal.fire(
+            "Error",
+            "Passwords do not match",
+            "error"
+        );
+
+        return;
+    }
+
+    const { error } =
+    await supabaseClient
+    .from("users")
+    .insert([
+        {
+            name,
+            mobile,
+            ko_code,
+            username,
+            password,
+            user_type
+        }
+    ]);
+
+    if(error){
+
+        Swal.fire(
+            "Error",
+            error.message,
+            "error"
+        );
+
+        return;
+    }
+
+    Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "User Created Successfully"
+    });
+
+});
