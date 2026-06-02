8eecb8f08a13b6a1479b5b741722cdfb2c347b34
SalinderArya <samajsevak2013@gmail.com>
2026-06-02 07:49:38 +0530

    Update admin.js

--- superadmin/js/admin.js
+++ superadmin/js/admin.js
@@ -5,3 +5,33 @@ function logout(){
     window.location.href = "../login/login.html";
 
 }
+
+const SUPABASE_URL =
+"https://hjxqtfykngkwmiejeyei.supabase.co";
+
+const SUPABASE_KEY =
+"Aapki Publishable Key Yahan";
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function loadUsers(){
+
+    const { data, error } =
+    await supabaseClient
+    .from("users")
+    .select("*")
+    .order("id", { ascending:false });
+
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    console.log(data);
+}
+
+loadUsers();
