651a02399404d9fed3beea40fcf548e9cbe40cc5
SalinderArya <samajsevak2013@gmail.com>
2026-06-02 07:59:28 +0530

    Update admin.js

--- superadmin/js/admin.js
+++ superadmin/js/admin.js
@@ -10,7 +10,7 @@ const SUPABASE_URL =
 "https://hjxqtfykngkwmiejeyei.supabase.co";
 
 const SUPABASE_KEY =
-"Aapki Publishable Key Yahan";
+"YAHAN_APNI_PUBLISHABLE_KEY_DALO";
 
 const supabaseClient =
 supabase.createClient(
@@ -24,14 +24,44 @@ async function loadUsers(){
     await supabaseClient
     .from("users")
     .select("*")
-    .order("id", { ascending:false });
+    .order("id",{ascending:false});
 
     if(error){
         console.log(error);
         return;
     }
 
-    console.log(data);
+    let html = "";
+
+    data.forEach(user => {
+
+        html += `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.mobile}</td>
+            <td>${user.ko_code}</td>
+            <td>${user.username}</td>
+            <td>${user.user_type}</td>
+            <td>${user.status}</td>
+
+            <td>
+                <button class="approve-btn">
+                    Approve
+                </button>
+
+                <button class="reject-btn">
+                    Reject
+                </button>
+            </td>
+        </tr>
+        `;
+    });
+
+    document.getElementById(
+        "userTableBody"
+    ).innerHTML = html;
 }
 
 loadUsers();
+
