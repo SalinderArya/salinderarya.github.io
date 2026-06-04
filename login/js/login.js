75d366a9dec37e8afaf578c4efa1e6dee5adfab5
SalinderArya <samajsevak2013@gmail.com>
2026-06-04 09:12:34 +0530

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,24 +1,75 @@
-alert("JS Loaded");
+const SUPABASE_URL =
+"https://hjxqtfykngkwmiejeyei.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_cspfS5CyrtJGlp9DdAq5Pw_DQJoevoG";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
 
 document.addEventListener(
 "DOMContentLoaded",
 function(){
 
 ```
-    alert("DOM Loaded");
-
-    const btn =
-    document.getElementById("loginBtn");
-
-    console.log(btn);
-
-    btn.onclick = function(){
-
-        alert("Button Working");
-
-    };
+    document
+    .getElementById("loginBtn")
+    .onclick = loginUser;
 
 }
 ```
 
 );
+
+async function loginUser(){
+
+```
+const username =
+document.getElementById("username")
+.value.trim();
+
+const password =
+document.getElementById("password")
+.value.trim();
+
+// SUPER ADMIN
+
+if(
+    username === "admin" &&
+    password === "admin"
+){
+
+    alert("Super Admin Login");
+
+    window.location.href =
+    "../superadmin/admin.html";
+
+    return;
+}
+
+// SUPABASE TEST
+
+const { data, error } =
+await supabaseClient
+.from("users")
+.select("*");
+
+console.log(data);
+console.log(error);
+
+if(error){
+
+    alert(error.message);
+    return;
+}
+
+alert(
+    "Users Found : " +
+    data.length
+);
+```
+
+}
