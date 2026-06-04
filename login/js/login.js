3066377981f12555c82d6d16ef9798d748b6cc29
SalinderArya <samajsevak2013@gmail.com>
2026-06-04 08:19:26 +0530

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -12,96 +12,37 @@ SUPABASE_KEY
 
 document
 .getElementById("loginBtn")
-.addEventListener("click", loginUser);
+.addEventListener("click", testLogin);
 
-async function loginUser(){
+async function testLogin(){
 
 ```
-const username =
-document.getElementById("username")
-.value.trim();
-
-const password =
-document.getElementById("password")
-.value.trim();
-
-// SUPER ADMIN
-
-if(
-    username === "admin" &&
-    password === "admin"
-){
-
-    Swal.fire(
-        "Success",
-        "Super Admin Login",
-        "success"
-    ).then(() => {
-
-        window.location.href =
-        "../superadmin/admin.html";
-
-    });
-
-    return;
-}
-
-// USER LOGIN
+alert("Button Clicked");
 
 const { data, error } =
 await supabaseClient
 .from("users")
 .select("*");
 
-console.log("ALL USERS =", data);
+console.log("DATA =", data);
 console.log("ERROR =", error);
 
+if(error){
 
     Swal.fire(
-        "Error",
-        "Invalid Username or Password",
+        "Database Error",
+        error.message,
         "error"
     );
 
     return;
 }
 
-if(data.status !== "approved"){
-
-    Swal.fire(
-        "Pending",
-        "Account Not Approved Yet",
-        "warning"
-    );
-
-    return;
-}
-
-localStorage.setItem(
-    "userId",
-    data.id
-);
-
-localStorage.setItem(
-    "userName",
-    data.name
-);
-
-localStorage.setItem(
-    "userType",
-    data.user_type
-);
-
 Swal.fire(
     "Success",
-    "Login Successful",
+    "Database Connected",
     "success"
-).then(() => {
-
-    window.location.href =
-    "../dashboard/dashboard.html";
-
-});
+);
 ```
 
 }
